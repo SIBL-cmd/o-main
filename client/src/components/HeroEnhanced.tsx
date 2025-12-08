af822f37845fd1bc1291220931ca3431dce93597
SIBL-cmd <bnbwebbusiness@gmail.com>
2025-12-08 20:21:40 +0300

    new5

--- client/src/components/HeroEnhanced.tsx
+++ client/src/components/HeroEnhanced.tsx
@@ -464,7 +464,7 @@ export default function HeroEnhanced() {
     </div>
   </div>
 
-  {/* Существующая кнопка SWAP (оставь как есть) */}
+  {/* Существующая кнопка SWAP (оставь как есть)  */}
 
 
               
